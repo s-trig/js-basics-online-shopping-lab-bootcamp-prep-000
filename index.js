6c2a3e33746f28e41f01843aac46f7ae1450b8a2
David Reep <davereep@gmail.com>
2017-10-10 23:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,76 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random() * 100) + 1)
+  cart.push({ [item]: price })
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
+  const l = cart.length
+
+  if (l === 0){
+    return console.log("Your shopping cart is empty.")
+  }
+
+  const itemsAndPrices = []
+
+  for(var i = 0; i < l; i++) {
+    let itemsAndPrice = cart[i]
+    let item = Object.keys(itemsAndPrice)
+    let price = itemsAndPrice[item]
+
+    itemsAndPrices.push(`${item} at $${price}`)
+  }
+
+  var comma = itemsAndPrices.join(', ')
+  var and = itemsAndPrices.join(' and ')
+
+  if (itemsAndPrices.length > 2) {
+    console.log(`In your cart, you have ${comma}`)
+  }
+  else
+   console.log(`In your cart, you have ${and}.`)
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  var array = []
+
+  for(var i = 0; i < cart.length; i++) {
+    let itemsAndPrice = cart[i]
+    let item = Object.keys(itemsAndPrice)
+    let price = itemsAndPrice[item]
+
+    array.push(price)
+    total += array[i]
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemInCart = false
+    for(var i = 0; i < cart.length; i++) {
+      if(cart[i].hasOwnProperty(item)) {
+        itemInCart = true
+      cart = cart.slice(0, i).concat(cart.slice(i+1))
+    }
+  }
+    if(!itemInCart) {
+    console.log("That item is not in your cart.")
+  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+    return cart
+  }
+
 }
